bc7c865f320cb46dd1900a64ced751bb57cff6a2
Alesandro Silva <59420110+alleSilva@users.noreply.github.com>
2020-12-26 19:49:48 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import Dashboard from './pages/Dashboard';
 import GlobalStyles from './styles/GlobalStyles';
 const App: React.FC = ()=> {
   return (
-		<>
-			<GlobalStyles/>
-			<Dashboard/>
-    </>
+	<>
+	  <GlobalStyles/>
+	  <Dashboard/>
+    	</>
   );
 }
 
